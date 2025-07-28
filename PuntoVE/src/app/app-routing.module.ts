e1cecf452b800562e6a59fbd83d13cd4fbf91216
Admingett <gettmex@gmail.com>
2025-07-27 21:26:57 -0600

    2508-PVAng-InicioColavoradores-PV

--- PuntoVE/src/app/app-routing.module.ts
+++ PuntoVE/src/app/app-routing.module.ts
@@ -52,6 +52,14 @@ const routes: Routes = [
     path: 'panlel-controll-admin',
     loadChildren: () => import('./pages/panlel-controll-admin/panlel-controll-admin.module').then( m => m.PanlelControllAdminPageModule),
      
+  },,
+  {
+    path: 'auth-users',
+    loadChildren: () => import('./auth/auth-users/auth-users.module').then( m => m.AuthUsersPageModule)
+  },
+  {
+    path: 'punto-venta',
+    loadChildren: () => import('./pages/punto-venta/punto-venta.module').then( m => m.PuntoVentaPageModule)
   },
 ];
 
